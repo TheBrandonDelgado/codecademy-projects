92c1cf50308f98fd8a71c6781f823cf2d91ad973
Brandon Delgado <TheBrandonDelgado@gmail.com>
2023-09-16 07:34:24 -0500

    Add Meal Maker
    Learn JavaScript Syntax: Objects

---
+++ Front-End Engineer Career Path/Building Interactive Websites/JavaScript Syntax, Part II/Meal Maker/app.js
@@ -0,0 +1,23 @@
+const menu = {
+    _meal: 'Spaghetti',
+    _price: 5,
+    set meal(mealToCheck) {
+      if (typeof mealToCheck === 'string') {
+        this._meal = mealToCheck;
+      }
+    },
+    set price(priceToCheck) {
+      if (typeof priceToCheck === "number") {
+        this._price = priceToCheck;
+      }
+    },
+    get todaysSpecial() {
+      if (this._meal && this._price) {
+        return `Today's special is ${this._meal} for $${this._price}!`;
+      } else {
+        return 'Meal or price was not set correctly!';
+      }
+    }
+  };
+  
+  console.log(menu.todaysSpecial);
