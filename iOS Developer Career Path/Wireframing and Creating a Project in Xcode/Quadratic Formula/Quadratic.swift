a7d3505b818120c2a0ccf0c550241d049dfa59fb
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-01-26 15:56:26 -0600

    Add Quadratic Formula
    Variables

---
+++ iOS Developer Career Path/Wireframing and Creating a Project in Xcode/Quadratic Formula/Quadratic.swift
@@ -0,0 +1,9 @@
+var a: Double = 3
+var b: Double = -11
+var c: Double = -4
+
+var root1: Double = (-b + (b * b - 4 * a * c).squareRoot()) / (2 * a)
+var root2: Double = (-b - (b * b - 4 * a * c).squareRoot()) / (2 * a)
+
+print(root1)
+print(root2)
---
+++ iOS Developer Career Path/Wireframing and Creating a Project in Xcode/Quadratic Formula/README.md
@@ -0,0 +1,2 @@
+# Quadratic Formula
+Write a Swift program in Quadratic.swift that solves the quadratic equation for the x‘s.
