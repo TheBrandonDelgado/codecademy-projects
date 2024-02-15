52a17e36e5e43cf8f366d69d9e6e51a710cbd9e4
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-02-15 17:26:14 -0600

    Add Morse Decoder
    Dictionaries

---
+++ iOS Developer Career Path/Building Code History: The User Interface/Morse Decoder/Morse.swift
@@ -0,0 +1,80 @@
+var englishText = "this is a secret message"
+
+var secretMessage = ".... --- .-- -.. -.--   .--. .- .-. - -. . .-."
+
+// Add your code below 🤫
+let letterToMorse = [
+  "A": ".-",
+  "B": "-...",
+  "C": "-.-.",
+  "D": "-..",
+  "E": ".",
+  "F": "..-.",
+  "G": "--.",
+  "H": "....",
+  "I": "..",
+  "J": ".---",
+  "K": "-.-",
+  "L": ".-..",
+  "M": "--",
+  "N": "-.",
+  "O": "---",
+  "P": ".--.",
+  "Q": "--.-",
+  "R": ".-.",
+  "S": "...",
+  "T": "-",
+  "U": "..-",
+  "V": "...-",
+  "W": ".--",
+  "X": "-..-",
+  "Y": "-.--",
+  "Z": "--.."
+]
+var morseText = ""
+var decodedMessage = ""
+var morseCodeArray = [String]()
+var currMorse = ""
+var morseToLetter: [String: String] = [:]
+
+for element in englishText {
+  let formattedElement = element.uppercased()
+  if let morseChar = letterToMorse["\(formattedElement)"] {
+    morseText += morseChar + " "
+  } else {
+    morseText += "   "
+  }
+}
+
+for char in secretMessage {
+  if char != " " {
+    currMorse += "\(char)"
+  } else {
+    switch currMorse {
+      case "":
+        currMorse += " "
+      case " ":
+        morseCodeArray.append(" ")
+        currMorse = ""
+      default:
+        morseCodeArray.append(currMorse)
+        currMorse = ""
+    }
+  }
+}
+
+morseCodeArray.append(currMorse)
+
+for (letter, morseChar) in letterToMorse {
+  morseToLetter[morseChar] = letter
+}
+
+for morseValue in morseCodeArray {
+  if let letterChar = morseToLetter[morseValue] {
+    decodedMessage += letterChar
+  } else {
+    decodedMessage += " "
+  }
+}
+
+print(decodedMessage)
---
+++ iOS Developer Career Path/Building Code History: The User Interface/Morse Decoder/README.md
@@ -0,0 +1,9 @@
+# Morse Decoder
+Morse code, named after American artist Samuel Finley Breese Morse, is a method of telecommunication that visually encodes letters into unique combinations of dots . and dashes -
+
+Morse code has been used as a communication aide in aviation, radio, assistive technology, and more.
+
+Here are a few rules to know when deciphering a Morse code message:
+* Each character is separated by a single space.
+* Each word is separated by three spaces.
+* Capitalization does not matter.
